4ca56288e5e81f1b17fa52321ed086ece5314289
nyan9 <rynnaing@gmail.com>
2021-02-10 16:38:21 -0500

    finished rspec for before validation of ensure_session_token

--- keep/app/models/user.rb
+++ keep/app/models/user.rb
@@ -1,12 +1,26 @@
 class User < ApplicationRecord
-  validates :username, presence: true
   validates :password_digest, presence: true
+  validates :session_token, :username, presence: true, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
+
+  before_validation :ensure_session_token
 
   attr_reader :password
 
+  def self.generate_session_token
+    token = SecureRandom::urlsafe_base64
+  end
+  
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+  
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
     @password = password
   end
+
+  
+
+  
 end
