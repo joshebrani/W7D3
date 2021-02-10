47db9159f7384a3d348939925797203e01d0feff
joshebrani <joshuaebrani@gmail.com>
2021-02-10 15:37:57 -0500

    Before deleting vendor and tmp files

--- keep/app/models/user.rb
+++ keep/app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   validates :username, presence: true
   validates :password_digest, presence: true
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 6, allow_nil: true}
 
-  attr_accessor :password
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
 end
--- keep/spec/models/user_spec.rb
+++ keep/spec/models/user_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # let!(:user) { User.new("asdf", "123123")}
+  subject(:user) { User.new(username: "asdf", password: "1234567")}
   
 
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
+  it { should allow_value(nil).for(:password)}
+
+  it "creates password_digest when password is given" do
+    expect(user.password_digest).to_not be_nil
+  end
+
 end
